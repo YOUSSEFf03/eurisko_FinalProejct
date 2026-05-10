a1c0a40ae12ca7ef8fb4c67604a5af1d425051e2
YOUSSEFf03 <202110800@ua.edu.lb>
2026-05-10 16:37:18 +0300

    chore(kafka): add shared topics and event interfaces

---
+++ libs/common/src/constants/kafka-topics.ts
@@ -0,0 +1,7 @@
+export const KAFKA_TOPICS = {
+  NOTIFICATION_OTP_SEND: 'notification.otp.send',
+  NOTIFICATION_WALLET_CREDITED: 'notification.wallet.credited',
+  NOTIFICATION_TRADE_EXECUTED: 'notification.trade.executed',
+  NOTIFICATION_PRICE_ALERT: 'notification.price.alert',
+  NOTIFICATION_CMS_PROVISIONED: 'notification.cms.provisioned',
+} as const;
